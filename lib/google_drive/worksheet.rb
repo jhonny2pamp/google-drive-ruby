48f6642b9d51ce675ac6271b3973992c30712b05
Jhonny Pamponet <allanjhonny1993@gmail.com>
2020-11-03 13:38:14 -0300

    fix

--- lib/google_drive/worksheet.rb
+++ lib/google_drive/worksheet.rb
@@ -339,10 +339,10 @@ module GoogleDrive
     end
 
     def rows_each_n(start_with, n)
-      # nc = num_cols_each_n(1, 20)
+      nc = num_cols_each_n(1, 9)
       result = (start_with..(start_with + n)).map do |row|
         puts self
-        (1..9).map { |col| self[row, col] }.freeze
+        (1..nc).map { |col| self[row, col] }.freeze
       end
       result.freeze
     end
